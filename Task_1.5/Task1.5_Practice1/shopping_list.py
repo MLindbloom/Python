cdc6030bf171afd0d037689dd248ac92681ff247
Matthew <106117475+MLindbloom@users.noreply.github.com>
2024-05-16 21:41:07 -0700

    create Task 1.5 Practice Test 1

---
+++ Task_1.5/Task1.5_Practice1/shopping_list.py
@@ -0,0 +1,41 @@
+class ShoppingList(object):
+  def __init__(self, list_name):
+    self.list_name = list_name
+    self.shopping_list = []
+
+  def add_item(self, item):
+    self.item = item
+    if item not in self.shopping_list:
+      self.shopping_list.append(item)
+      print(item, 'was added to shopping list')
+    else: 
+      print(item, 'is already on list')
+
+  def remove_item(self, item):
+    self.item = item
+    if item in self.shopping_list:
+      self.shopping_list.remove(item)
+      print(item, 'was removed from list')
+    else: 
+      print(item, 'is not in shopping list')
+
+  def view_list(self):
+    print('Shopping List', self.list_name + ':')
+    print('-----------------------------------')
+    item = self.item
+    for item in self.shopping_list:
+      print(item)
+
+pet_store_list = ShoppingList('Pet Store Shopping List')
+
+pet_store_list.add_item('dog food')
+pet_store_list.add_item('frisbee')
+pet_store_list.add_item('bowl')
+pet_store_list.add_item('collar')
+pet_store_list.add_item('flea collar')
+
+pet_store_list.remove_item('flea collar')
+
+pet_store_list.add_item('frisbee')
+
+pet_store_list.view_list()
