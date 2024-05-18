58715e66d208f0e540ecb654bdd53335027c358b
Matthew <106117475+MLindbloom@users.noreply.github.com>
2024-05-17 21:35:19 -0700

    fix: code cleanup

--- Task_1.5/Task1.5_Practice3/height.py
+++ Task_1.5/Task1.5_Practice3/height.py
@@ -24,14 +24,10 @@ class Height(object):
     height_inches_B = other.feet * 12 + other.inches
     return height_inches_A < height_inches_B
 
-
-
   def __le__(self, other):
       height_inches_A = self.feet * 12 + self.inches
       height_inches_B = other.feet * 12 + other.inches
       return height_inches_A <= height_inches_B
-
-
 
   def __eq__(self, other):
       height_inches_A = self.feet * 12 + self.inches
